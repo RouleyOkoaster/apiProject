5b484dd6184d93d8d0197a0c6e581ca10b95cfdb
GridFariz <gridfariz@gmail.com>
2026-07-22 02:31:44 +0300

    Initial commit

---
+++ tests/test_google_maps_api.py
@@ -0,0 +1,21 @@
+import pytest
+from utils.api import Google_maps_api
+
+"""Создание, изменение и удаление новой локации"""
+class TestCreatePlace():
+
+    def test_create_new_place(self):
+        print("Метод POST")
+        result_post = Google_maps_api.create_new_place() # Вызов метода по созданию новой локации
+
+        check_post = result_post.json()
+        place_id = check_post.get('place_id')
+
+        print("Method GET to check POST")
+        result_get = Google_maps_api.get_new_place(place_id)
+
+        print("Method PUT")
+        result_put = Google_maps_api.put_new_place(place_id)
+
+        print("Method GET to check PUT")
+        result_get = Google_maps_api.get_new_place(place_id)
