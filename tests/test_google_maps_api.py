1b10d8e77d452fa61da080b8b14723a8ce422f89
GridFariz <gridfariz@gmail.com>
2026-07-23 22:25:57 +0300

    Added check_json_value

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -29,6 +29,7 @@ class TestCreatePlace():
 
         Checking.check_status_code(result_post, 200)
         Checking.check_json_token(result_post, list(token))
+        Checking.check_json_value(result_post, "status", "OK")
 
 
         print("Method GET to check POST")
@@ -62,28 +63,33 @@ class TestCreatePlace():
 
         Checking.check_status_code(result_get, 200)
         Checking.check_json_token(result_get, list(token))
+        Checking.check_json_value(result_get, "address", token["address"])
 
         print("Method PUT")
         result_put = Google_maps_api.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
         Checking.check_json_token(result_put, ["msg"])
+        Checking.check_json_value(result_put, "msg", "Address successfully updated")
 
         print("Method GET to check PUT")
         result_get = Google_maps_api.get_new_place(place_id)
 
         Checking.check_status_code(result_get, 200)
         Checking.check_json_token(result_get, list(token))
+        Checking.check_json_value(result_get, "address", "100 Lenina street, RU")
 
         print("Method DELETE")
         result_delete = Google_maps_api.delete_place(place_id)
 
         Checking.check_status_code(result_delete, 200)
         Checking.check_json_token(result_delete, ["status"])
+        Checking.check_json_value(result_delete, "status", "OK")
 
         print("Method GET to check DELETE")
         result_get = Google_maps_api.get_new_place(place_id)
 
         Checking.check_status_code(result_get, 404)
         Checking.check_json_token(result_get, ["msg"])
+        Checking.check_json_value(result_get, "msg", "Get operation failed, looks like place_id  doesn't exists")
 
 
--- utils/checking.py
+++ utils/checking.py
@@ -17,4 +17,11 @@ class Checking():
         assert list(token) == expected_value, f"Expected {expected_value} but got {token}"
         print(f"Values in json format of response as expected ({expected_value})")
 
+    """Метод для проверки значений обязательных полей в ответе запроса"""
+    @staticmethod
+    def check_json_value(result, field_name, expected_value):
+        check = result.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value, f"Expected {expected_value} but got {check_info}"
+        print(f"Value of field {field_name} in json format of response as expected ({expected_value})")
 
